436b37cc1318e64eec80ae2062d2b8934a69052b
Dar-ya218 <dasharoura13465@gmail.com>
2024-04-21 08:43:17 +0200

    revers end

--- actions/reverse_rotate.c
+++ actions/reverse_rotate.c
@@ -26,3 +26,22 @@ static void	reverse_rotate(t_stack **stack)
 	*stack = second_last->next;
 	second_last->next = NULL;
 }
+
+void	rra(t_stack **a)
+{
+	reverse_rotate(a);
+	ft_printf("rra\n");
+}
+
+void	rrb(t_stack **b)
+{
+	reverse_rotate(b);
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	reverse_rotate(a);
+	reverse_rotate(b);
+	ft_printf("rrr\n");
+}
