0ae96df6c1372d19ecc8a8cdaaed3c461a542d13
Dar-ya218 <dasharoura13465@gmail.com>
2024-04-21 08:42:44 +0200

    revers

--- actions/reverse_rotate.c
+++ actions/reverse_rotate.c
@@ -9,5 +9,20 @@
 /*   Updated: 2024/04/16 14:32:59 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "../../includes/push_swap.h"
 
+static void	reverse_rotate(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*second_last;
 
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ;
+	first = *stack;
+	second_last = *stack;
+	while (second_last->next->next != NULL)
+		second_last = second_last->next;
+	second_last->next->next = first;
+	*stack = second_last->next;
+	second_last->next = NULL;
+}
