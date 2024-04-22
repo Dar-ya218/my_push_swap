ea2fb05139a2321481b7b6e5fedddc59b29b5d65
Dar-ya218 <dasharoura13465@gmail.com>
2024-04-22 17:43:17 +0200

    start rotate

--- actions/rotate.c
+++ actions/rotate.c
@@ -9,5 +9,20 @@
 /*   Updated: 2024/04/16 14:32:13 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "../../includes/push_swap.h"
 
+static void	rotate(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack	*second;
+	t_stack	*lastnode;
 
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ;
+	first = *stack;
+	second = (*stack)->next;
+	lastnode = ft_stacklast(*stack);
+	*stack = second;
+	lastnode->next = first;
+	first->next = NULL;
+}
