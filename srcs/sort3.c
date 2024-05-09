ea864e8966c9b3335b95076f16d9c3435ccb8f8d
Dar-ya218 <dasharoura13465@gmail.com>
2024-05-09 10:21:27 +0200

    Implement Find Max Function"

---
+++ srcs/sort3.c
@@ -0,0 +1,26 @@
+#include "../includes/push_swap.h"
+
+/** This function finds the max node in the list and returns a pointer to it
+ * It does so by first assigning the min long value to the max, ensuring
+ * no node->number will be lower and then proceeds to iterate, updating
+ * whenever a larger number is found.
+**/
+t_stack	*find_max(t_stack *stack)
+{
+	long	max;
+	t_stack	*max_node;
+
+	if (stack == NULL)
+		return (NULL);
+	max = LONG_MIN;
+	while (stack)
+	{
+		if (stack->number > max)
+		{
+			max = stack->number;
+			max_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (max_node);
+}
