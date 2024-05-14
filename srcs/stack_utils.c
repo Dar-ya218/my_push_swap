36015281c20bf4beac6a27e3927bc07f4dcafcac
dabochko <dabochko@student.42barcelona.com>
2024-05-14 17:01:15 +0200

    Add Stack Sorting Check and Node Creation Functions

--- srcs/stack_utils.c
+++ srcs/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: dabochko <dabochko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:13:31 by dabochko          #+#    #+#             */
-/*   Updated: 2024/05/14 14:57:59 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/05/14 14:58:46 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,18 @@ int	is_sorted(t_stack **stack)
 	return (1);
 }
 
+/** This function allocates memory for a new node and
+ * stores the content in the node, then setting the
+ * next to NULL as this will be the new last node.
+**/
+t_stack	*ft_stacknew(long content)
+{
+	t_stack	*new;
 
+	new = (t_stack *)malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->number = content;
+	new->next = NULL;
+	return (new);
+}
