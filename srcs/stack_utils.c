4c16bc02718cfc9caa6095772ff6a959aad92666
dabochko <dabochko@student.42barcelona.com>
2024-05-14 14:58:21 +0200

    Add Stack Sorting Check Function

--- srcs/stack_utils.c
+++ srcs/stack_utils.c
@@ -3,12 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dabochko <dabochko@student.42barcel>       +#+  +:+       +#+        */
+/*   By: dabochko <dabochko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 14:13:31 by dabochko          #+#    #+#             */
-/*   Updated: 2024/05/09 14:15:13 by dabochko         ###   ########.fr       */
+/*   Updated: 2024/05/14 14:57:59 by dabochko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/push_swap.h"
+
+/** Checks whether a stack is already sorted 
+ * It iterates through the list and checks whether the 
+ * current number is bigger than the next number
+**/
+int	is_sorted(t_stack **stack)
+{
+	t_stack	*head;
+
+	head = *stack;
+	while (head && head->next)
+	{
+		if (head->number > head->next->number)
+			return (0);
+		head = head->next;
+	}
+	return (1);
+}
 
 
